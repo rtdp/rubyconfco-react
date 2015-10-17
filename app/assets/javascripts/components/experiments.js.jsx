600069d764f141d8bab7052a3814d4e78924c0c5
Ratnadeep Deshmane <ratnadeepdeshmane@gmail.com>
2015-10-17 09:42:19 -0500

    v5

--- app/assets/javascripts/components/experiments.js.jsx
+++ app/assets/javascripts/components/experiments.js.jsx
@@ -1,8 +1,8 @@
 var App = React.createClass({
   render: function() {
     return <div>
-      <h1>Hello Medellin</h1>
-      <span>Beautiful mountainous city.</span>
+      <h1 style={{color:'blue', fontStyle: 'italic'}}>Hello Medellin</h1>
+      <span className='tagline'>Beautiful mountainous city.</span>
     </div>
   }
 });
