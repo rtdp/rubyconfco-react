255113725cbb263bb5635768072a9182871ac46a
Ratnadeep Deshmane <ratnadeepdeshmane@gmail.com>
2015-10-17 09:45:01 -0500

    v8

--- app/assets/javascripts/components/experiments.js.jsx
+++ app/assets/javascripts/components/experiments.js.jsx
@@ -1,15 +1,23 @@
 var cities = [
-  { name: 'Medellin', tagline: 'Beautiful mountainous city.' },
-  { name: 'Bogota', tagline: 'The capitol.' },
-  { name: 'Barranquilla', tagline: 'The costal city.' }
+  { name: 'Medellin', tagline: 'Beautiful mountainous city.', rainy: true },
+  { name: 'Bogota', tagline: 'The capitol.', rainy: false },
+  { name: 'Barranquilla', tagline: 'The costal city.', rainy: false }
 ];
 
 var App = React.createClass({
   render: function() {
     var places = this.props.cities.map(function(city){
+      var rainyState;
+      if(city.rainy){
+        rainyState = <span> And rainy too.</span>
+      }else{
+        rainyState = <span> Not rainy.</span>
+      }
+
       return <div>
-        <h1>{city.name}</h1>
+        <h1 className={city.rainy ? 'rainy' : ''}>{city.name}</h1>
         <span>{city.tagline}</span>
+        {rainyState}
         <hr/>
       </div>
     });
