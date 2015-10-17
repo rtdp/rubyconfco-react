ba0890da67887a743f71d69ec5612826a8264af7
Ratnadeep Deshmane <ratnadeepdeshmane@gmail.com>
2015-10-17 09:40:41 -0500

    v3

--- app/assets/javascripts/components/experiments.js.jsx
+++ app/assets/javascripts/components/experiments.js.jsx
@@ -1,1 +1,10 @@
-React.render(React.createElement('h1', null, 'Hello Medellin'), document.body);
+var App = React.createClass({
+  render: function() {
+    return React.createElement('div',
+      null,
+      React.createElement('h1', null, 'Hello Medellin'),
+      React.createElement('span', null, 'Beautiful mountainous city.'))
+  }
+});
+
+React.render(React.createElement(App, null), document.body);
